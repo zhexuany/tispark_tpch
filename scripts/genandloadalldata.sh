6de4625b112c8af0b99ff00659508138e5ba0e88
zhexuany <yangzhexuan@pingcap.com>
2017-09-08 14:55:53 -0500

    uncomment some lines

--- scripts/genandloadalldata.sh
+++ scripts/genandloadalldata.sh
@@ -13,9 +13,9 @@ check_dbgen_binary() {
 check_dbgen_binary
 cat $META_DIR/tables | awk -F ' ' '{print$3}' | while read table; do
     echo "creating database and tables $table"
-    # bash create.sh
-    # echo "generating tables $table"
-    # bash gen.sh "$table"
-    # echo "loading tables $table"
-    # bash load.sh "$table"
+    bash create.sh
+    echo "generating tables $table"
+    bash gen.sh "$table"
+    echo "loading tables $table"
+    bash load.sh "$table"
     done
